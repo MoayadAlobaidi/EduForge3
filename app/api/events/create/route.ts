d4bb61b66bc8619abfde072df930caa5a40808ef
MoayadAlobaidi <107114309+MoayadAlobaidi@users.noreply.github.com>
2025-10-08 16:13:54 +0700

    Update route.ts

--- app/api/events/create/route.ts
+++ app/api/events/create/route.ts
@@ -1,6 +1,6 @@
+// create
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
-
 export async function POST(req: Request) {
   const b = await req.json();
   const e = await prisma.event.create({
