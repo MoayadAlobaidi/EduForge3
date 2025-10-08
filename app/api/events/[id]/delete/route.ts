847c82d52cd88abc1a1f0d2790476717d513e35f
MoayadAlobaidi <107114309+MoayadAlobaidi@users.noreply.github.com>
2025-10-08 16:14:15 +0700

    Update route.ts

--- app/api/events/[id]/delete/route.ts
+++ app/api/events/[id]/delete/route.ts
@@ -1,6 +1,6 @@
+// delete
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
-
 export async function POST(_: Request, { params }: { params: { id: string } }) {
   await prisma.event.delete({ where: { id: params.id }});
   return NextResponse.json({ ok: true });
